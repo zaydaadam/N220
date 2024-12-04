3b1a92e6c1675fe946bab6c341964563410cec85
Dahir <93958704+MugenGm@users.noreply.github.com>
2024-12-03 23:53:58 -0500

    fixed

--- homework-seven/broken_JS.js
+++ homework-seven/broken_JS.js
@@ -7,33 +7,28 @@ function RPS(numb) {
 
   document.getElementById("resultDecision").innerHTML =
     "The computer chose: " + CompDecision;
-  if (numb === 3) {
-    innerscore -= 0.5;
+
+  if (numb === 0) {
+    if (CompDecision === "Scissors") {
+      innerscore += 1; 
+    } else if (CompDecision === "Paper") {
+      innerscore -= 1; 
+    }
   }
 
   if (numb === 1) {
-    if (CompDecision === "Paper") {
-      innerscore -= 1;
-    }
-    if (CompDecision === "Scissors") {
-      innerscore += 1;
-    }
-  }
-
-  if (numb === 0) {
     if (CompDecision === "Rock") {
-      innerscore += 1;
-    }
-    if (CompDecision === "Scissors") {
-      innerscore -= 1;
+      innerscore += 1; 
+    } else if (CompDecision === "Scissors") {
+      innerscore -= 1; 
     }
   }
 
   if (numb === 2) {
-    if (CompDecision === "Rock") {
-      innerscore -= 1;
-    } else if (CompDecision === "Paper") {
-      innerscore += 1;
+    if (CompDecision === "Paper") {
+      innerscore += 1; 
+    } else if (CompDecision === "Rock") {
+      innerscore -= 1; 
     }
   }
 
